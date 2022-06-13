bf3b55f86bca8ff5c49f817e8f29e687330ed8ed
Alessandragr <alessandra-ribeiro@live.com>
2022-06-13 10:23:52 -0700

    Exercício 2

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,6 +1,6 @@
 public class Main {
 	public static void main (String[] args) {
 		float pi = 3.14159f;
-		System.out.println("Valor de pi: %.3f", pi);
+		System.out.print("Valor de pi: %.3f", pi);
 	}
 }
