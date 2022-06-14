7c4e8f49c41c298493fe800c458e880d36b965c6
Alessandragr <alessandra-ribeiro@live.com>
2022-06-14 10:34:09 -0700

    Atividade 0

--- TiposPrimitivos_Wrapper/inteiro_positivo/Program.java
+++ TiposPrimitivos_Wrapper/inteiro_positivo/Program.java
@@ -1,33 +1,29 @@
 public class InteiroPositivo {
-	public static void main(String[] args) {
-		 try {
-			 private int valor;
-			 public Numero (int valor) {
-				  Integer valor =  new Integer(2);
-				  ehInteiro();
-			 }
-			 public Texto (String valor) {
-				 Integer valor =  new Integer("2");
-				 ehInteiro();
-			 }
-		 } catch (IllegalArgumentException e) {
-			 System.out.println(e);
-		 }
-		 public ehInteiro (int valor) {
-			 try (Integer.parseInt(valor)) {
-				 System.out.println("Valor eh um valor inteiro positivo");
-			 } catch (IllegalArgumentException e) {
-				 System.out.println("Valor nao eh um valor inteiro positivo");
-			 }
-		 }
-		 public ehPrimo (int valor) {
-			 for (int i=2; i<valor; i++) {
-				  if(valor%i==0) {
-					  return true;
-				  } else {
-					  return false;
-				  }
-			 }
-		 }
+	public static void main (String[] args) {
+		private int valor;
+
+		public InteiroPositivo (int valor) {
+			this.valor = Integer.valueOf(valor);
+		}
+		public InteiroPositivo (String valor) {
+			this.valor = Integer.parseInt(valor);
+		}
+		public ehInteiro (int valor) {
+			try (Integer.parseInt(valor)) {
+				System.out.println("Valor eh um valor inteiro positivo");
+			} catch (IllegalArgumentException e) {
+				System.out.println("Valor nao eh um valor inteiro positivo");
+			}
+		}
+		Public ehPrimo (int valor) {
+			for (int i=2; i<valor; i++) {
+				if(valor%i==0) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		}
 	}
 }
+
