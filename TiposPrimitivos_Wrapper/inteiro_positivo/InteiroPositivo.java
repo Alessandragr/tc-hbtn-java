1b724eb60aded8e215c1464c15ea292e181f4b27
Alessandragr <alessandra-ribeiro@live.com>
2022-06-14 17:10:17 -0300

    Atividade 0

--- TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
+++ TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -1,34 +1,36 @@
 public class InteiroPositivo {                                                                                                                                                                                                                    
-        private int valor;                                                                                               
+    private int valor;                                                                                               
                                                                                                                                                   
-        public InteiroPositivo (int valor) {                                                                                              
-            this.valor = Integer.valueOf(valor);                                                                                      
-        }                                                                                                                                 
-        public InteiroPositivo (String valor) {                                                                                           
-            this.valor = Integer.parseInt(valor);                                                                                     
-        }
-        public int getValor() {
-            return valor;
-        }
-        public int setValor(int valor) {
-            this.valor = valor;
-        }                                                                  
+    public InteiroPositivo (int valor) {                                                                                              
+        this.valor = Integer.valueOf(valor);                                                                                      
+    }                                                                                                                                 
+    public InteiroPositivo (String valor) {                                                                                           
+        this.valor = Integer.parseInt(valor);                                                                                     
+    }
+    public int getValor() {
+        return valor;
+        ehInteiro();
+    }
+    public int setValor(int valor) {
+        this.valor = valor;
+        ehInteiro();
+    }                                                                  
                                                                                                                                                   
-        public int ehInteiro(int valor) {                                                                                                 
-            if(valor >= 0) {                                                                                            
-                return valor;                                                                                                     
-            } else {                                                                                    
-                throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");                                                     
-            }                                                                                                                         
-        } 
+    public int ehInteiro(int valor) {                                                                                                 
+        if(valor >= 0) {                                                                                            
+            return valor;                                                                                                     
+        } else {                                                                                    
+            throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");                                                     
+        }                                                                                                                         
+    } 
 
-        public boolean ehPrimo () {                                                                                                      
-            for (int i=2; i<valor; i++) {                                                                                             
-                if(valor%i==0 || valor%i==1) {                                                                                                  
-                    return true;                                                                                              
-                } else {                                                                                                          
-                    return false;                                                                                             
-                }                                                                                                                 
-            }                                                                                                                         
-        }                                                                                                                                
+    public boolean ehPrimo () {                                                                                                      
+        for (int i=2; i<valor; i++) {                                                                                             
+            if(valor%i==0 || valor%i==1) {                                                                                                  
+                return true;                                                                                              
+            } else {                                                                                                          
+                return false;                                                                                             
+            }                                                                                                                 
+        }                                                                                                                         
+    }                                                                                                                                
 }                                                                                                                                         
