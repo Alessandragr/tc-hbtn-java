001d0410ce69de5c35c18612566901ede8b65e6b
Alessandragr <alessandra-ribeiro@live.com>
2022-06-14 15:53:51 -0300

    Atividade 0

--- TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
+++ TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -23,7 +23,7 @@ public class InteiroPositivo {
             }                                                                                                                         
         } 
 
-        public ehPrimo (int valor) {                                                                                                      
+        public boolen ehPrimo (int valor) {                                                                                                      
             for (int i=2; i<valor; i++) {                                                                                             
                 if(valor%i==0) {                                                                                                  
                     return true;                                                                                              
