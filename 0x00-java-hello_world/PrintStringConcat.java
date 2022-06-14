70ee4779378fe0a681331524c4a01a3a827373ce
Alessandragr <alessandra-ribeiro@live.com>
2022-06-13 17:56:59 -0700

    Exercicio 4

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -3,6 +3,6 @@ public class Main {
 	       String texto1 = "Holberton";
 	       String texto2 = "School";
 	       String texto3 = ("Bem vindo a " + texto1 + " " + texto2 +"!");
-	 	System.out.printf(texto3);
+	 	System.out.println(texto3);
 	}
 }	
