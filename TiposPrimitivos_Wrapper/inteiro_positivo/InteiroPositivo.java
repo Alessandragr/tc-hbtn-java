b2273319ee87065072da954416c8f7dd179ea03b
Alessandragr <alessandra-ribeiro@live.com>
2022-06-14 17:17:12 -0300

    Atividade 0

--- TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
+++ TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -26,6 +26,7 @@ public class InteiroPositivo {
             } else {                                                                                                          
                 return false;                                                                                             
             }                                                                                                                 
-        }                                                                                                                         
+        } 
+        return true;                                                                                                                        
     }                                                                                                                                
 }                                                                                                                                         
