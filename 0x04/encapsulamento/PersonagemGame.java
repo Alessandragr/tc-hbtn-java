720bcb42a14ff551be3347efff6b5b2c7e38ba76
Alessandragr <alessandra-ribeiro@live.com>
2022-06-15 10:13:15 -0300

    Atividade 1

---
+++ 0x04/encapsulamento/PersonagemGame.java
@@ -0,0 +1,22 @@
+public class PersonagemGame {
+    public int saudeAtual;
+    public String nome;
+
+
+    public int getSaudeAtual() {
+        return this.saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
