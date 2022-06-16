1e450c08c0db159169b40cfe9bf4abcad8a84c18
Alessandragr <alessandra-ribeiro@live.com>
2022-06-16 18:38:35 -0300

    Atividade 0

---
+++ 0x09/agregacao/Empregado.java
@@ -0,0 +1,36 @@
+
+
+public class Empregado {
+    private String nome;
+    private int codigo;
+    private Endereco endereco;
+
+    public Empregado(String nome, int codigo, Endereco endereco) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.endereco = endereco;
+    }
+
+   public void apresentar() {
+    System.out.println( "Codigo: " + this.codigo + "\n" + "Nome: " + this.nome + "\n" + 
+            "Bairro: " + endereco.getBairro() + "\n" + "Cidade: " + endereco.getCidade() + "\n" + "Pais: " + endereco.getPais()
+    		);
+   }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+}
---
+++ 0x09/agregacao/Program.java
@@ -0,0 +1,21 @@
+public class Program {
+    public static void main(String[] args) {
+    	//System.out.println("comecou");
+        Endereco endereco = new Endereco("Rua Marechal Deodoro",
+                "135",
+                "",
+                "Santo Amaro",
+                "Sao Paulo",
+                "SP",
+                "Brasil");
+        //System.out.println("passou do endereco");
+
+        Empregado empregado = new Empregado("Jane Doe", 1, endereco);
+        //System.out.println("passou do empregado");
+
+        empregado.apresentar();
+        //System.out.println("passou do apresentar");
+
+        System.out.println();
+    }
+}
