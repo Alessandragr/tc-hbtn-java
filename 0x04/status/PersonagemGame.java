3d362c28cc2d525d2e8b77c1d4f5b9cefdb888a8
Alessandragr <alessandra-ribeiro@live.com>
2022-06-16 10:01:38 -0300

    Atividade 3

---
+++ 0x04/status/PersonagemGame.java
@@ -0,0 +1,51 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+
+    public int getSaudeAtual() {
+        return this.saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+
+        if (this.saudeAtual>0) {
+            this.status = "vivo";
+        } else {
+            this.status = "morto";
+        }
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public int tomarDano (int quantidadeDeDano) {
+        this.saudeAtual -= quantidadeDeDano;
+        if(this.saudeAtual<=0) {
+            return this.saudeAtual=0;
+        } else {
+            return this.saudeAtual;
+        }
+    }
+    public int receberCura (int quantidadeDeCura) {
+        this.saudeAtual += quantidadeDeCura;
+        if(this.saudeAtual>=100) {
+            return this.saudeAtual=100;
+        } else {
+            return this.saudeAtual;
+        }
+    }
+   
+    
+}
