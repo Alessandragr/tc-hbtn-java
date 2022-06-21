69dd038c00c029963c3a694a0efe93b52018eb0d
Alessandragr <alessandra-ribeiro@live.com>
2022-06-21 16:30:34 -0300

    Atividade 1

---
+++ 0x01/pares/NumerosPares.java
@@ -0,0 +1,9 @@
+
+public class NumerosPares {
+    public static void main(String[] args) {
+        for(int i=0; i<97; i+=2) {
+            System.out.printf(i + ", ");
+        }
+        System.out.println(98);
+    }
+}
