c8e60f1d0f222eba94a968a3e507a4c65fda1519
Alessandra Ribeiro <68918402+Alessandragr@users.noreply.github.com>
2022-07-06 14:07:15 -0300

    Update ProcessadorVideo.java

--- Interfaces/workflow/ProcessadorVideo.java
+++ Interfaces/workflow/ProcessadorVideo.java
@@ -2,14 +2,14 @@ import java.util.ArrayList;
 
 public class ProcessadorVideo {
 
-    private ArrayList<CanalNotificacao> canalNotificacao = new ArrayList<>();
+    private ArrayList<CanalNotificacao> canais = new ArrayList<>();
 
     public void registrarCanal(CanalNotificacao canalNotificacao) {
-        this.canalNotificacao.add(canalNotificacao);
+        this.canais.add(canalNotificacao);
     }
 
     public void processar(Video video) {
-        for (CanalNotificacao canal : canalNotificacao) {
+        for (CanalNotificacao canal : canais) {
             canal.notificar(new Mensagem(video.getArquivo() + " - " + video.getFormato(), TipoMensagem.LOG));
 
         }
