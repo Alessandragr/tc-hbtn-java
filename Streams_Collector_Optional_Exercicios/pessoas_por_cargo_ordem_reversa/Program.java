7b22c06f2011c18bbfa1c8d15123033177ba2861
Alessandragr <alessandra-ribeiro@live.com>
2022-07-18 00:49:32 -0300

    Atividade 0 - 4

--- Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Program.java
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Program.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class Program {
     public static void main(String[] args) {
+    	final var locale = new Locale("pt", "BR"); 
+    	Locale.setDefault(locale);
         Pessoa pessoa1 = new Pessoa(1, "John Bannins", "Desenvolvedor", 23, 3200);
         Pessoa pessoa2 = new Pessoa(2, "Derry Collier", "Desenvolvedor", 45, 8600);
         Pessoa pessoa3 = new Pessoa(3, "Mikey Berg", "Product Owner", 51, 11350);
